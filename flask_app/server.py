f9682f2f84eaa903eaa47cb3848d35452f9d7729
Logan Beard <loganbeardnz@gmail.com>
2022-04-28 11:45:11 +1200

    restructure app in flask style

--- flask_app/server.py
+++ flask_app/server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from nba_api_gateway import get_graph_data
+from .nba_api_gateway import get_graph_data
 
 app = Flask(__name__)
 
@@ -8,7 +8,3 @@ app = Flask(__name__)
 @app.route("/standings", methods=['GET'])
 def graph_data():
     return get_graph_data()
-
-
-if __name__ == "__main__":
-    app.run()
---
+++ run.py
@@ -0,0 +1,5 @@
+from flask_app.server import app
+
+
+if __name__ == "__main__":
+    app.run()
